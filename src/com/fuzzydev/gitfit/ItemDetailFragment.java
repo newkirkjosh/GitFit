f6c3f372a08edad6ac82a4abd016e8fa60add2b9
DejanRistic <dejanr23@gmail.com>
2012-07-06 14:14:57 -0400

    commit

---
+++ src/com/fuzzydev/gitfit/ItemDetailFragment.java
@@ -0,0 +1,28 @@
+package com.fuzzydev.gitfit;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemDetailFragment extends Fragment {
+
+
+    public ItemDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+        //Set content and stuff
+        return rootView;
+    }
+}
