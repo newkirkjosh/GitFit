ab03a87dc8de81470738e2a7dc260a919a6ef212
DejanRistic <dejanr23@gmail.com>
2012-07-16 14:17:56 -0400

    Modified Manifest to allow Internet access, and updated CloudManager for testing

--- src/com/fuzzydev/gitfit/cloud/CloudManager.java
+++ src/com/fuzzydev/gitfit/cloud/CloudManager.java
@@ -1,33 +1,37 @@
 package com.fuzzydev.gitfit.cloud;
 
-import com.fuzzydev.gitfit.structures.User;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Looper;
-import android.util.Log;
-
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.params.CoreProtocolPNames;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
 import org.json.JSONObject;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Looper;
+import android.util.Log;
 
+import com.fuzzydev.gitfit.structures.User;
 
 public class CloudManager {
-	
+
 	private static CloudManager manager = null;
 	private static Context currentContext;
 	InputStream in = null;
 
-	private CloudManager() {}
-	
+	private CloudManager() {
+	}
+
 	public static CloudManager getInstance(Context context) {
 		if (manager == null)
 			manager = new CloudManager();
@@ -42,37 +46,61 @@ public class CloudManager {
 	public static void setCurrentContext(Context currentContext) {
 		CloudManager.currentContext = currentContext;
 	}
-	
+
 	public void registerUser(User user) {
 		new registerUserWithCloud().execute(user);
 	}
-	
-	class registerUserWithCloud extends AsyncTask<User,Void,Void>{
+
+	class registerUserWithCloud extends AsyncTask<User, Void, Void> {
 
 		@Override
 		protected Void doInBackground(User... params) {
 			User temp = params[0];
-			 Looper.prepare(); //For Preparing Message Pool for the child Thread
-             HttpClient client = new DefaultHttpClient();
-             HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
-             HttpResponse response;
-             JSONObject json = new JSONObject();
-             try{
-                 HttpPost post = new HttpPost("http://gitfittemp.appspot.com/gitfitserver/GitFitServerServlet");
-                 json.put("firstName", temp.getFirstName());
-                 StringEntity se = new StringEntity( json.toString());  
-                 se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-                 post.setEntity(se);
-                 response = client.execute(post);
-                 /*Checking response */
-                 String httpStatus = response.getStatusLine().toString();
-                 Log.v("Response :", httpStatus);
-             }
-             catch(Exception e){
-                 e.printStackTrace();
-             }
-             Looper.loop(); //Loop in the message queue
-             return null;
-         }
-	}	
+			HttpClient client = new DefaultHttpClient();
+		    client.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Custom user agent");
+
+			HttpConnectionParams
+					.setConnectionTimeout(client.getParams(), 50000); // Timeout
+																		// Limit
+			HttpResponse response;
+			JSONObject json = new JSONObject();
+
+			HttpPost post = new HttpPost(
+					"http://gitfittemp.appspot.com/gitfitserverservlet");
+			try {
+				Log.v("firstName: ",temp.getFirstName());
+				json.put("firstName", temp.getFirstName());
+			} catch (JSONException e2) {
+				e2.printStackTrace();
+			}
+
+			StringEntity se = null;
+			try {
+				se = new StringEntity(json.toString());
+			} catch (UnsupportedEncodingException e1) {
+				e1.printStackTrace();
+			}
+
+			se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,
+					"application/json"));
+			post.setEntity(se);
+			try {
+				response = client.execute(post);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				Log.v("StatusCode : ",statusCode + "");
+				if (statusCode == 200) {
+					Log.d("DEBUG", "status code = 200");
+				} else {
+					Log.e(CloudManager.class.toString(),
+							"Failed");
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
 }
