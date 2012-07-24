14c76d16bacf6ec6b4b73b1169fd4a68503f89a1
DejanRistic <dejanr23@gmail.com>
2012-07-24 16:22:56 -0400

    attatched login activity, there is a gradient issue, it looks banded

--- src/com/fuzzydev/gitfit/GFLogInActivity.java
+++ src/com/fuzzydev/gitfit/GFLogInActivity.java
@@ -1,15 +1,43 @@
 package com.fuzzydev.gitfit;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.fuzzydev.gitfit.GFLogInFragment.Callbacks;
 
-public class GFLogInActivity extends SherlockFragmentActivity{
+public class GFLogInActivity extends Activity implements Callbacks{
+
+	Button login;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gflogin_layout);
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
 
 	@Override
-	protected void onCreate(Bundle arg0) {
-		// TODO Auto-generated method stub
-		super.onCreate(arg0);
-		setContentView(R.layout.gflogin_layout);
+	public void onItemSelected(String id) {
+		//Callback from GFLoginFragment
+		
+		if(id.equalsIgnoreCase("login")){
+			startActivity(new Intent(GFLogInActivity.this,GFMainListActivity.class));
+		}
+		else if(id.equalsIgnoreCase("register")){
+			
+		}
 	}
 }
--- src/com/fuzzydev/gitfit/GFLogInFragment.java
+++ src/com/fuzzydev/gitfit/GFLogInFragment.java
@@ -6,12 +6,36 @@ import com.fuzzydev.gitfit.GFMainListFragment.Callbacks;
 import com.fuzzydev.gitfit.R;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 public class GFLogInFragment extends Fragment{
 	
+	Button login;
+	Button register;
+	View myView;
+    private Callbacks callbacks = loginCallbacks;
+
+	
+    public interface Callbacks {
+
+        public void onItemSelected(String id);
+    }
+
+    private static Callbacks loginCallbacks = new Callbacks() {
+    	
+        @Override
+        public void onItemSelected(String id) {
+        	
+        }
+    };
 	
 	 public GFLogInFragment() {
 	    }
@@ -19,23 +43,47 @@ public class GFLogInFragment extends Fragment{
 	    @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-
 	    }
-
 	    @Override
 	    public View onCreateView(android.view.LayoutInflater inflater, android.view.ViewGroup container, Bundle savedInstanceState) {
 		    // Inflate the layout for this fragment
-	        return inflater.inflate(R.layout.gflogin_layout, container, false);
+	    	
+	        myView = inflater.inflate(R.layout.gflogin_fragment, container, false);
+	        
+			login = (Button)myView.findViewById(R.id.login);
+			login.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					callbacks.onItemSelected("login");			
+				}
+			});
+			
+			register = (Button)myView.findViewById(R.id.register);
+			register.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					callbacks.onItemSelected("register");
+				}
+			});
+			
+			return myView;
 	    };
 
 	    @Override
 	    public void onAttach(Activity activity) {
 	        super.onAttach(activity);
+	        if (!(activity instanceof Callbacks)) {
+	            throw new IllegalStateException("Activity must implement fragment's callbacks.");
+	        }
 
+	        callbacks = (Callbacks) activity;
 	    }
 
 	    @Override
 	    public void onDetach() {
 	        super.onDetach();
+	        callbacks = loginCallbacks;
 	    }
 }
